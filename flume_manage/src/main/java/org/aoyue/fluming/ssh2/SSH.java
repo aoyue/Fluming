95386a46187752663ce3fc7d82dbbf3cc574b63e
clicoy <cxx152@163.com>
2015-01-20 23:35:43 +0800

    use project in idea.

---
+++ flume_manage/src/main/java/org/aoyue/fluming/ssh2/SSH.java
@@ -0,0 +1,11 @@
+package org.aoyue.fluming.ssh2;
+
+/**
+ * This interface is use to connection all the know server,
+ * <br>user need to input server IP address and username/password.
+ * I think coding is fun.
+ * Created by clicoy on 15-1-20.
+ */
+public interface SSH {
+
+}
---
+++ flume_manage/src/main/java/org/aoyue/fluming/utils/Utils.java
@@ -0,0 +1,12 @@
+package org.aoyue.fluming.utils;
+
+/**
+ * This Class is Util.so some usefully things.<br/>
+ * I think it is useful.
+ * Created by clicoy on 15-1-20.
+ */
+public class Utils {
+    public static void justtest(){
+        System.out.println("Just test and a demo class.");
+    }
+}
